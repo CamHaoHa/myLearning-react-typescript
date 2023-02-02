2399c0aee9d21aa03b1b4a39d3f4bb483baabfdc
CamHaoHa <camhaovn@gmail.com>
2023-02-03 08:35:34 +1100

    event handler typescript

--- src/state/SearchUser.tsx
+++ src/state/SearchUser.tsx
@@ -1,34 +1,56 @@
-import React , {useState} from 'react'
-
+import React from 'react'
+import { useState } from 'react'
 type Props = {}
 
 const SearchUser:React.FC = () => {
-  const users = [
-    {name: 'Andrew',age: 100},
-    {name: 'Cathie', age: 1000},
-    {name: 'Andrew2', age:10000}
-  ]
-  const [name, setName ] = useState('')
-  const [foundUser, setUser] = useState<{name:string, age: number} | undefined>()
-  const handleSearch = () => {
-    const foundUser = users.find((user) => {return user.name=== name})
-    setUser(foundUser)
-  }
+  const users = [ {
+    name: 'admin', email: 'admin@admin.com', power: true
+  },
+  {name: 'staff', email: 'staff@staff.com', power: false}
+]
+
+const [email,setEmail] = useState('')
+const [foundUser,setSearchUser] = useState<{name:string,email:string, power:boolean} | undefined>()
+
+const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+  setEmail(e.target.value)
+}
+
+const handleSearch:React.MouseEventHandler<HTMLButtonElement> = () => {
+  const foundUser = users.find(user => {return user.email === email})
+  setSearchUser(foundUser)
+}
+
+const handleDrag = (event: React.DragEvent<HTMLDivElement>) => {
+  console.log(event)
+}
   return (
+
     <div>
-        <h3>Another example with state: Search User</h3>
-        <input 
-          type="text" 
-          value = {name} 
-          onChange={(e) => setName(e.target.value)
-        } />
-        <button
-          type='button'
-          onClick={handleSearch}
-        >
-          SearchUser
-        </button>
-        {foundUser && (<div>{foundUser.name}{foundUser.age}</div>) }
+      <h3>Search Users</h3>
+      <input 
+        type='text' 
+        value={email}
+        onChange={handleChange}
+      />
+      <button
+        type='button'
+        onClick={handleSearch}  
+      >
+        Search...
+      </button>
+      <div>
+        {foundUser && (
+          <div>
+            {foundUser.name}
+          </div>
+        )}
+      </div>
+      <div draggable
+        onDragStart={handleDrag}
+      >
+          <h1>DragMe</h1>
+      </div>
     </div>
   )
 }

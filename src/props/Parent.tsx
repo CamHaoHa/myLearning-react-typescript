b0cb00b421d93c8c619d6594ca25e493808ab23a
CamHaoHa <camhaovn@gmail.com>
2023-02-02 19:19:23 +1100

    children prop

--- src/props/Parent.tsx
+++ src/props/Parent.tsx
@@ -4,7 +4,14 @@ import React from 'react'
 
 const Parent = () => {
   return (
-    <Child color='red'/>
+    <Child color='123' onClick ={() =>
+      console.log('clicked')
+    }>
+        <div>
+          Children prop 
+          {/* by default, this text right here would show up inside of our child component as a prop call children */}
+        </div>
+    </Child>
   )
 }
 
--- src/props/child.tsx
+++ src/props/child.tsx
@@ -1,9 +1,27 @@
 interface ChildProp {
   color: string
+  onClick : () => void
+  children?: React.ReactNode  
 }
 
-export const Child = ({color}:ChildProp) => {
+// like java void is used wher there is no data. like a function doesnot return any value
+
+
+export const Child = ({color, onClick}:ChildProp) => {
   return <div>
     {color}
+    <button onClick ={onClick}>Click Me</button>
   </div>
 }
+
+
+ //differenrt way 
+
+export const ChildasFc: React.FC<ChildProp> =({color, onClick}) => {
+  return <div>{color}
+  
+  <button onClick ={onClick}>Click Me</button></div>
+  
+
+}
+

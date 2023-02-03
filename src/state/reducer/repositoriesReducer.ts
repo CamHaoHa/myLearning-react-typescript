480376860f679f47ffa53604cd8cea87390f1ce3
CamHaoHa <camhaovn@gmail.com>
2023-02-03 15:47:14 +1100

    refactor code

--- src/state/reducer/repositoriesReducer.ts
+++ src/state/reducer/repositoriesReducer.ts
@@ -3,19 +3,40 @@ interface RepositoriesState {
   error: string | null
   data: string[]
 }
-interface Action {
-  type: string,
-  payload ?: any
+interface SearchRepositoriesAction{
+  type: 'search_repositories';
+}
+
+interface SearchRepositoriesSuccessAction {
+  type: 'search_repositories_success';
+  payload: string[]
+}
+
+type Action = 
+  |SearchRepositoriesAction
+  |SearchRepositoriesSuccessAction
+  |SearchRepositoriesErrorAction
+interface SearchRepositoriesErrorAction {
+  type: 'search_repositories_error';
+  payload:string 
+}
+
+enum ActionType {
+  SEARCH_REPOSITORIES = 'search_repositories',
+  SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success',
+  SEARCH_REPOSITORIES_ERROR = 'search_repositories_error'
+
 }
 const reducer = (state:RepositoriesState, action: Action):RepositoriesState => {
-  switch(action.type) {
-    case 'search_repositories' :
-      return {loading: true, error: null, data:[]}
-    case 'search_repositories_success':
-      return {loading: false, error: null, data:action.payload}
-    case 'search_repositories_error':
-      return {loading: false, error:action.payload, data:[]}
-    default: return state
+
+    switch(action.type) {
+      case ActionType.SEARCH_REPOSITORIES :
+        return {loading: true, error: null, data:[]}
+      case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+        return {loading: false, error: null, data:action.payload}
+      case ActionType.SEARCH_REPOSITORIES_ERROR:
+        return {loading: false, error:action.payload, data:[]}
+      default: return state
   }
 }
 
